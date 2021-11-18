f1dcb431ddf0f8b51dc9c60020e72852f343d32f
EvgenyiBardik <groove8478@gmail.com>
2021-11-18 22:41:25 +0200

    Initial commit.

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,13 +75,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django_db',
-        'USER' : 'user_name',
-        'PASSWORD' : 'password',
-        'HOST' : '127.0.0.1',
-        'PORT' : '5432',    }
+    "default": {
+        "ENGINE" : "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+    }
 }
 
 
